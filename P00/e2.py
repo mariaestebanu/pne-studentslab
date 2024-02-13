3ad19c7d93d798d160d05b7fc4a52492815a7e0f
mestu <m.estebanu.2023@alumnos.urjc.es>
2024-02-13 16:38:38 +0100

    This is going to be my first but not last commit

--- P00/e2.py
+++ P00/e2.py
@@ -1,12 +1,7 @@
-def seq_read_fasta(filename):
-    from pathlib import Path
-
-    # -- Constant with the new of the file to open
-
-
-    # -- Open and read the file
-    file_contents = Path(filename).read_text()
-    list_contents = file_contents.split('\n')
-    for i in range(1, len(list_contents)):
-        print(list_contents[i])
-seq_read_fasta( "../sequences/U5.fa")
+from Seq0 import *
+final_list = ['../sequences/U5_sequence.fa', '../sequences/ADA_sequence.fa','../sequences/FRAT1_sequence.fa','../sequences/FXN_sequence.fa','../sequences/RNU6_269P_sequence.fa']
+final_name_list = ['Gene U5','Gene ADA','Gene FRAT1','Gene FXN','Gene RNU6_269P']
+for i in final_list:
+    num = final_list.index(i)
+    length = seq_len(i)
+    print(final_name_list[num], '--> Length:', length)
